ec0d3c97617590150edf1f4e27dc18a9d66c7c0b
Vlad Vlad <AreXDiv@gmail.com>
2023-11-17 18:47:23 +0700

    Ограничение ввода в 20 символов в имени бренда.

--- LarekLib/Models/Brand.cs
+++ LarekLib/Models/Brand.cs
@@ -1,8 +1,12 @@
-﻿namespace LarekLib.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LarekLib.Models
 {
     public class Brand
     {
         public int Id { get; set; }
+
+        [MaxLength(20)]
         public string? Name { get; set; }
     }
 }
